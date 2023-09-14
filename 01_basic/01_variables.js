fc677f10f6a92e72ed43d3925cac1d8fb4b3c531
Sagar Adhikari <42830742+ssagar-adhikari@users.noreply.github.com>
2023-09-14 01:37:38 +0000

    variable done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1455444;
+let accountEmail = "ssagar1016@gmail.com"
+var accountPassword = "123456"
+city = "lalbandi" //treat as variable but not good practice 
+let accountState;
+
+
+ //accountId = "123" //not allowed to change cont vaiable 
+accountEmail = "sa@sa.com";
+accountPassword= "123"
+city="sarlahi"
+
+/*
+Prefer Not To Use Var beacuse of  issue in block scope and function scope
+*/
+
+// console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,city,accountState]);
