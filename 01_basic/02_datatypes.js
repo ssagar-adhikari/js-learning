07cf5bb6ec1105ddc0bba547856a87cd31eed7a9
Sagar Adhikari <42830742+ssagar-adhikari@users.noreply.github.com>
2023-09-21 02:36:50 +0000

    datatypes basic

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code a newer version --no need to specify but its a good practice
+
+// alert(3+3) // we are using node js 
+
+// https://tc39.es/  //official site of ecma script
+
+let name = "sagar" //string
+let age = 28 ; //integer
+
+let isLoggedIn = true //boolean
+
+// number => 2 to power 53
+//bigInt
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => value is not assigned
+//symbol => used for unique
+//object
+
+console.log(typeof "asasa");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
