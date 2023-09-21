0be2e86a4915d75e8f72779365b8ed6761a9aabe
Sagar Adhikari <42830742+ssagar-adhikari@users.noreply.github.com>
2023-09-21 15:42:51 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 33
+score = "33a"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//conversion to number
+
+//"33" => 33 treat as a number
+//"33avs" => NaN ==> string to number
+//undefined => Nan =>undefined to number
+//Null => 0 => mull value to number
+//true => 1 => boolean value to number
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//Conversion to boolean
+
+// 1 => true 0 => false
+//"" => false
+//"sdas" => true
+// null =>false
+// undefined =>false
+
+//To String conversion
+let num = 33
+let stringValue = String(num)
+console.log(typeof stringValue)
+console.log(stringValue)
